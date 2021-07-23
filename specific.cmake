3a6dcaddec96fed7d8ce5b62e69a0f7097c023b1
Wai-Shing Luk <luk036@gmail.com>
2021-07-23 19:33:30 +0800

    add back the libraries

--- specific.cmake
+++ specific.cmake
@@ -1,3 +1,23 @@
+CPMAddPackage(
+  NAME fmt
+  GIT_TAG 7.1.3
+  GITHUB_REPOSITORY fmtlib/fmt
+  OPTIONS "FMT_INSTALL YES" # create an installable target
+)
+
 CPMAddPackage("gh:microsoft/GSL@3.1.0")
-CPMAddPackage("gh:xtensor-stack/xtl#0.7.2")
-CPMAddPackage("gh:xtensor-stack/xtensor#0.23.10")
+
+CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
+if(xtl_ADDED)
+  message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
+  include_directories(${xtl_SOURCE_DIR}/include)
+endif(xtl_ADDED)
+
+CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
+if(xtensor_ADDED)
+  message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
+  include_directories(${xtensor_SOURCE_DIR}/include)
+endif(xtensor_ADDED)
+
+set(SPECIFIC_LIBS fmt::fmt GSL)
+# remember to turn off the warnings
