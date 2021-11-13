579eda5fbe4e83c0e6fa776ccc542c0f51cf74a8
Wai-Shing Luk <luk036@gmail.com>
2021-11-13 04:56:23 +0000

    reformat using --target fix-format

--- specific.cmake
+++ specific.cmake
@@ -1,19 +1,23 @@
-CPMAddPackage(
-  NAME fmt
-  GIT_TAG 7.1.3
-  GITHUB_REPOSITORY fmtlib/fmt
-  OPTIONS "FMT_INSTALL YES" # create an installable target
+cpmaddpackage(
+  NAME
+  fmt
+  GIT_TAG
+  7.1.3
+  GITHUB_REPOSITORY
+  fmtlib/fmt
+  OPTIONS
+  "FMT_INSTALL YES" # create an installable target
 )
 
-CPMAddPackage("gh:microsoft/GSL@3.1.0")
+cpmaddpackage("gh:microsoft/GSL@3.1.0")
 
-CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
+cpmaddpackage("gh:xtensor-stack/xtl#0.6.23")
 if(xtl_ADDED)
   message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
   include_directories(${xtl_SOURCE_DIR}/include)
 endif(xtl_ADDED)
 
-CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
+cpmaddpackage("gh:xtensor-stack/xtensor#0.22.0")
 if(xtensor_ADDED)
   message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
   include_directories(${xtensor_SOURCE_DIR}/include)
