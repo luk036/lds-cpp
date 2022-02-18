de11833dfe9409abc779b892e8d5b44d8b2a3015
Wai-Shing Luk <luk036@gmail.com>
2022-02-18 22:16:58 +0800

    add const

--- test/source/test_lds.cpp
+++ test/source/test_lds.cpp
@@ -1,31 +1,31 @@
 #include <doctest/doctest.h>  // for Approx, ResultBuilder, TestCase, CHECK
 
 #include <gsl/span>     // for span
-#include <lds/lds.hpp>  // for circle, halton, sphere, sphere3_hopf
+#include <lds/lds.hpp>  // for Circle, Halton, Sphere, Sphere3Hopf
 
 TEST_CASE("Circle") {
     auto cgen = lds::Circle(2);
-    auto [x, y] = cgen.pop();
+    const auto [x, y] = cgen.pop();
     CHECK(x == doctest::Approx(0.0));
 }
 
 TEST_CASE("Halton") {
-    size_t base[] = {2, 3};
+    const size_t base[] = {2, 3};
     auto hgen = lds::Halton(base);
-    auto [x, y] = hgen.pop();
+    const auto [x, y] = hgen.pop();
     CHECK(x == doctest::Approx(0.5));
 }
 
 TEST_CASE("Sphere") {
-    size_t base[] = {2, 3};
+    const size_t base[] = {2, 3};
     auto sgen = lds::Sphere(base);
-    auto [s0, s1, s2] = sgen.pop();
+    const auto [s0, s1, s2] = sgen.pop();
     CHECK(s0 == doctest::Approx(0.8660254038));
 }
 
 TEST_CASE("Sphere3Hopf") {
-    size_t base[] = {2, 3, 5};
+    const size_t base[] = {2, 3, 5};
     auto shfgen = lds::Sphere3Hopf(base);
-    auto [s0, s1, s2, s3] = shfgen.pop();
+    const auto [s0, s1, s2, s3] = shfgen.pop();
     CHECK(s0 == doctest::Approx(-0.2236067977));
 }
