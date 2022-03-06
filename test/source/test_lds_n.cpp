3f1c955ba95613d4103b8711c195c14d837761ba
Wai-Shing Luk <luk036@gmail.com>
2022-03-06 18:10:48 +0800

    fix by trunk

--- test/source/test_lds_n.cpp
+++ test/source/test_lds_n.cpp
@@ -1,8 +1,8 @@
 #include <doctest/doctest.h>  // for Approx, ResultBuilder, TestCase
 
-#include <gsl/span>                 // for span
+#include <gsl/span>       // for span
 #include <lds/lds_n.hpp>  // for cylin_n, halton_n, sphere3, sphere_n
-#include <vector>                   // for vector
+#include <vector>         // for vector
 
 TEST_CASE("Sphere3") {
     const size_t base[] = {2, 3, 5};
