fcdbe63075e7a6cdf467150bb1e5cc4a102acf2b
Wai-Shing Luk <luk036@gmail.com>
2023-03-01 20:23:20 +0800

    separate lds and sphere-n

--- include/lds/greeter.h
+++ include/lds/greeter.h
@@ -4,28 +4,28 @@
 
 namespace lds {
 
-    /**  Language codes to be used with the Lds class */
-    enum class LanguageCode { EN, DE, ES, FR };
+/**  Language codes to be used with the Lds class */
+enum class LanguageCode { EN, DE, ES, FR };
 
-    /**
-     * @brief A class for saying hello in multiple languages
-     */
-    class Lds {
-        std::string name;
+/**
+ * @brief A class for saying hello in multiple languages
+ */
+class Lds {
+  std::string name;
 
-      public:
-        /**
-         * @brief Creates a new xn
-         * @param name the name to greet
-         */
-        Lds(std::string name);
+public:
+  /**
+   * @brief Creates a new xn
+   * @param name the name to greet
+   */
+  Lds(std::string name);
 
-        /**
-         * @brief Creates a localized string containing the greeting
-         * @param lang the language to greet in
-         * @return a string containing the greeting
-         */
-        std::string greet(LanguageCode lang = LanguageCode::EN) const;
-    };
+  /**
+   * @brief Creates a localized string containing the greeting
+   * @param lang the language to greet in
+   * @return a string containing the greeting
+   */
+  std::string greet(LanguageCode lang = LanguageCode::EN) const;
+};
 
-}  // namespace lds
+} // namespace lds
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,55 +1,55 @@
-#include <lds/greeter.h>  // for Lds
-#include <lds/version.h>  // for LDS_VERSION
+#include <lds/greeter.h> // for Lds
+#include <lds/version.h> // for LDS_VERSION
 
-#include <cxxopts.hpp>    // for value, OptionAdder, Options, OptionValue
-#include <iostream>       // for string, operator<<, endl, basic_ostream
-#include <memory>         // for shared_ptr
-#include <string>         // for char_traits, hash, operator==
-#include <unordered_map>  // for operator==, unordered_map, __hash_map_const...
-#include <utility>        // for pair
+#include <cxxopts.hpp>   // for value, OptionAdder, Options, OptionValue
+#include <iostream>      // for string, operator<<, endl, basic_ostream
+#include <memory>        // for shared_ptr
+#include <string>        // for char_traits, hash, operator==
+#include <unordered_map> // for operator==, unordered_map, __hash_map_const...
+#include <utility>       // for pair
 
-auto main(int argc, char** argv) -> int {
-    const std::unordered_map<std::string, lds::LanguageCode> languages{
-        {"en", lds::LanguageCode::EN},
-        {"de", lds::LanguageCode::DE},
-        {"es", lds::LanguageCode::ES},
-        {"fr", lds::LanguageCode::FR},
-    };
+auto main(int argc, char **argv) -> int {
+  const std::unordered_map<std::string, lds::LanguageCode> languages{
+      {"en", lds::LanguageCode::EN},
+      {"de", lds::LanguageCode::DE},
+      {"es", lds::LanguageCode::ES},
+      {"fr", lds::LanguageCode::FR},
+  };
 
-    cxxopts::Options options(*argv, "A program to welcome the world!");
+  cxxopts::Options options(*argv, "A program to welcome the world!");
 
-    std::string language;
-    std::string name;
+  std::string language;
+  std::string name;
 
-    // clang-format off
+  // clang-format off
   options.add_options()
     ("h,help", "Show help")
     ("v,version", "Print the current version number")
     ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
     ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
   ;
-    // clang-format on
+  // clang-format on
 
-    auto result = options.parse(argc, argv);
+  auto result = options.parse(argc, argv);
 
-    if (result["help"].as<bool>()) {
-        std::cout << options.help() << std::endl;
-        return 0;
-    }
-
-    if (result["version"].as<bool>()) {
-        std::cout << "Lds, version " << LDS_VERSION << std::endl;
-        return 0;
-    }
-
-    auto langIt = languages.find(language);
-    if (langIt == languages.end()) {
-        std::cerr << "unknown language code: " << language << std::endl;
-        return 1;
-    }
-
-    // lds::Lds lds(name);
-    // std::cout << lds.greet(langIt->second) << std::endl;
-
+  if (result["help"].as<bool>()) {
+    std::cout << options.help() << std::endl;
     return 0;
+  }
+
+  if (result["version"].as<bool>()) {
+    std::cout << "Lds, version " << LDS_VERSION << std::endl;
+    return 0;
+  }
+
+  auto langIt = languages.find(language);
+  if (langIt == languages.end()) {
+    std::cerr << "unknown language code: " << language << std::endl;
+    return 1;
+  }
+
+  // lds::Lds lds(name);
+  // std::cout << lds.greet(langIt->second) << std::endl;
+
+  return 0;
 }
