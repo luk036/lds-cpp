ece13ab12edc56f8aedde7156abc36fbe563d5aa
Wai-Shing Luk <luk036@gmail.com>
2023-03-31 17:03:35 +0800

    remove include unnecessary gsl

--- specific.cmake
+++ specific.cmake
@@ -5,7 +5,7 @@ CPMAddPackage(
   OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
-# CPMAddPackage("gh:microsoft/GSL@3.1.0")
+CPMAddPackage("gh:microsoft/GSL@3.1.0")
 
-set(SPECIFIC_LIBS fmt::fmt)
+set(SPECIFIC_LIBS fmt::fmt GSL)
 # remember to turn off the warnings
