541ef35f2d5ae329ae4f2677466d349b49a57766
Wai-Shing Luk <luk036@gmail.com>
2026-05-24 22:18:39 +0800

    apply fix-format

--- source/logger.cpp
+++ source/logger.cpp
@@ -5,17 +5,17 @@
 
 namespace lds {
 
-void log_with_spdlog(const std::string& message) {
-    // Function-local static: created once on first call, reused thereafter
-    static auto logger = []() -> std::shared_ptr<spdlog::logger> {
-        auto log = spdlog::basic_logger_mt("file_logger", "lds.log");
-        log->set_level(spdlog::level::info);
-        log->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
-        log->flush_on(spdlog::level::info);
-        return log;
-    }();
-    logger->info("Lds message: {}", message);
-    logger->flush();
-}
+    void log_with_spdlog(const std::string& message) {
+        // Function-local static: created once on first call, reused thereafter
+        static auto logger = []() -> std::shared_ptr<spdlog::logger> {
+            auto log = spdlog::basic_logger_mt("file_logger", "lds.log");
+            log->set_level(spdlog::level::info);
+            log->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%n] [%^%l%$] %v");
+            log->flush_on(spdlog::level::info);
+            return log;
+        }();
+        logger->info("Lds message: {}", message);
+        logger->flush();
+    }
 
 }  // namespace lds
