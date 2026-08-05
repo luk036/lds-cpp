eb4f7583f6c6afb841b5831241be030dc532cd53
luk036 <luk036@users.noreply.github.com>
2026-08-05 08:44:37 +0000

    deploy: ad69aa5e0d629e2f16bd7eb7d5771c35fb2c8af6

--- search/functions_7.js
+++ search/functions_7.js
@@ -3,6 +3,6 @@ var searchData=
   ['operator_21_3d_0',['operator!=',['../classlds_1_1GeneratorIterator.html#a44987c81f766a00f07bc12ad7c993cc8',1,'lds::GeneratorIterator']]],
   ['operator_2a_1',['operator*',['../classlds_1_1GeneratorIterator.html#a3a60bb1a187bafec3f38416548e8be40',1,'lds::GeneratorIterator']]],
   ['operator_2b_2b_2',['operator++',['../classlds_1_1GeneratorIterator.html#a634339812a9e8c26bf4c2faf971fc3de',1,'lds::GeneratorIterator::operator++() -&gt; GeneratorIterator &amp;'],['../classlds_1_1GeneratorIterator.html#a2fa064de4c2d0a99bd0d43309016ecca',1,'lds::GeneratorIterator::operator++(int) -&gt; GeneratorIterator']]],
-  ['operator_3d_3',['operator=',['../classilds_1_1VdCorput.html#aa31a21a0b49d4c8ba75ae594146014ef',1,'ilds::VdCorput::operator=()'],['../classlds_1_1VdCorput.html#a5d97eda4bb7d54e8afa69885d0a21f06',1,'lds::VdCorput::operator=()']]],
+  ['operator_3d_3',['operator=',['../classlds_1_1VdCorputBase.html#a43b7b58ccf70bd1a38b5f35632242c4a',1,'lds::VdCorputBase::operator=(const VdCorputBase &amp;)=delete'],['../classlds_1_1VdCorputBase.html#a0056b1ea3658683dde11b9820569c25e',1,'lds::VdCorputBase::operator=(VdCorputBase &amp;&amp;)=delete']]],
   ['operator_3d_3d_4',['operator==',['../classlds_1_1GeneratorIterator.html#adcfcbf5fd6e1832b0e64ae17f19b35d5',1,'lds::GeneratorIterator']]]
 ];
--- search/functions_b.js
+++ search/functions_b.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['vdc_0',['vdc',['../namespacelds.html#ab68519d54a5c5bca52252ba76d07bd40',1,'lds']]],
   ['vdc2_5ftable_1',['vdc2_table',['../namespacelds.html#ac38d30e895686353ba6d221a45a1aa5b',1,'lds']]],
-  ['vdcorput_2',['vdcorput',['../classilds_1_1VdCorput.html#a1b736178d11220aedb7f052921bef501',1,'ilds::VdCorput::VdCorput(unsigned int scale=DEFAULT_SCALE)'],['../classilds_1_1VdCorput.html#ae3c1a545a331e643bf5ebffb04a2c87c',1,'ilds::VdCorput::VdCorput(VdCorput &amp;&amp;) noexcept=delete'],['../classlds_1_1VdCorput.html#a7e5ebd22112fd14b7d78b2b95b8b4098',1,'lds::VdCorput::VdCorput()'],['../classlds_1_1VdCorput.html#a2192667db8c134e81ca8fa9e98e6a1ca',1,'lds::VdCorput::VdCorput(VdCorput &amp;&amp;) noexcept=delete']]],
-  ['vdcorputdynamic_3',['VdCorputDynamic',['../classlds_1_1VdCorputDynamic.html#aa87a46b15fc1c136b94acd41b38f41ed',1,'lds::VdCorputDynamic']]],
-  ['vdcorputwrap_4',['VdCorputWrap',['../classlds_1_1VdCorputWrap.html#ada09afd2194ba8818fa45e7813a15595',1,'lds::VdCorputWrap']]]
+  ['vdcorput_2',['vdcorput',['../classilds_1_1VdCorput.html#a1b736178d11220aedb7f052921bef501',1,'ilds::VdCorput::VdCorput()'],['../classlds_1_1VdCorput.html#a7e5ebd22112fd14b7d78b2b95b8b4098',1,'lds::VdCorput::VdCorput()']]],
+  ['vdcorputbase_3',['vdcorputbase',['../classlds_1_1VdCorputBase.html#ab5a8b0d44ccd306046069196647e8874',1,'lds::VdCorputBase::VdCorputBase()=default'],['../classlds_1_1VdCorputBase.html#acaba108429b87db2ca61e1fecaf9ea64',1,'lds::VdCorputBase::VdCorputBase(const VdCorputBase &amp;)=delete'],['../classlds_1_1VdCorputBase.html#a2e956b35f6e3079fc32a4543e219f3d2',1,'lds::VdCorputBase::VdCorputBase(VdCorputBase &amp;&amp;)=delete']]],
+  ['vdcorputdynamic_4',['VdCorputDynamic',['../classlds_1_1VdCorputDynamic.html#aa87a46b15fc1c136b94acd41b38f41ed',1,'lds::VdCorputDynamic']]],
+  ['vdcorputwrap_5',['VdCorputWrap',['../classlds_1_1VdCorputWrap.html#ada09afd2194ba8818fa45e7813a15595',1,'lds::VdCorputWrap']]]
 ];
